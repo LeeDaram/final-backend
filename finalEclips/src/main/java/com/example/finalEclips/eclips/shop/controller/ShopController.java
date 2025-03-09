7a823bcd6a6fd286979160b9f4e669803891777a
Jhon.k <scpo2887@gmail.com>
2025-03-09 16:18:33 +0900

    강상윤, 박채영 Spring 연결

--- finalEclips/src/main/java/com/example/finalEclips/eclips/shop/controller/ShopController.java
+++ finalEclips/src/main/java/com/example/finalEclips/eclips/shop/controller/ShopController.java
@@ -3,7 +3,6 @@ package com.example.finalEclips.eclips.shop.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +13,10 @@ import com.example.finalEclips.eclips.shop.service.ShopService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-
-@RequestMapping("/api")
+@RequiredArgsConstructor
+@RequestMapping("/guides")
 public class ShopController {
  private final ShopService shopService;
- 
-
- public ShopController(ShopService shopService) {
-     this.shopService = shopService;
- }
  
  @GetMapping("/shop")
  public ResponseEntity<List<Shopdto>>getAllShops(){
