a016a72dcfeb37d8cc5175573cddeb00e0d5ad69
Jhon.k <scpo2887@gmail.com>
2025-03-09 16:12:19 +0900

    12
    12

---
+++ finalEclips/src/main/java/com/example/finalEclips/eclips/shop/controller/ShopController.java
@@ -0,0 +1,32 @@
+package com.example.finalEclips.eclips.shop.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.finalEclips.eclips.shop.dto.Shopdto;
+import com.example.finalEclips.eclips.shop.service.ShopService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+
+@RequestMapping("/api")
+public class ShopController {
+ private final ShopService shopService;
+ 
+
+ public ShopController(ShopService shopService) {
+     this.shopService = shopService;
+ }
+ 
+ @GetMapping("/shop")
+ public ResponseEntity<List<Shopdto>>getAllShops(){
+	 return ResponseEntity.ok(shopService.getAllShops());
+ }
+}
+
---
+++ finalEclips/src/main/java/com/example/finalEclips/eclips/shop/dto/Shopdto.java
@@ -0,0 +1,10 @@
+package com.example.finalEclips.eclips.shop.dto;
+
+import lombok.Data;
+
+@Data
+public class Shopdto {
+  private int industryId;
+  private String industryName;
+  private int count;
+}
