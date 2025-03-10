c0809cf6cc879deadb709a9b09f7a5f5eb706c17
thermoCat <jangsangmin0401@gmail.com>
2025-03-10 09:44:34 +0900

    2025-03-10
    착한업소등록 주방이미지 업로드

--- finalEclips/src/main/java/com/example/finalEclips/eclips/bizconfig/controller/BizImgConfigContrller.java
+++ finalEclips/src/main/java/com/example/finalEclips/eclips/bizconfig/controller/BizImgConfigContrller.java
@@ -1,14 +1,19 @@
 package com.example.finalEclips.eclips.bizconfig.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.finalEclips.eclips.bizconfig.dto.CreateApprovalDto;
 import com.example.finalEclips.eclips.bizconfig.dto.CreateGpbDto;
@@ -30,9 +35,16 @@ public class BizImgConfigContrller {
 		    response.put("storeId", gpbId); //프론트로 id값 반환해서 헤ㅠ 먼져 작성후 올라간 id값을 가져와서 승인관리 테이블만들기
 		    return ResponseEntity.ok(response);
 		}
-		@PostMapping("/approval/")
+		@PostMapping("/approval")
 		public ResponseEntity<CreateApprovalDto> createApproval(@RequestBody CreateApprovalDto approvalDto){
 			bizImgConfigService.createApproval(approvalDto);
 			return ResponseEntity.ok().build();
+		}
+		
+		@PostMapping("/approval/attachment")
+		public ResponseEntity<Void> createApprovalAttachment(@RequestPart("files") List<MultipartFile> files,
+				@RequestParam("storeId") int storeId){
+			bizImgConfigService.createApprovalAttachment(storeId, files);
+			return ResponseEntity.status(HttpStatus.CREATED).build();
 		}
 }
--- finalEclips/src/main/java/com/example/finalEclips/eclips/bizconfig/service/BizImgConfigServiceImpl.java
+++ finalEclips/src/main/java/com/example/finalEclips/eclips/bizconfig/service/BizImgConfigServiceImpl.java
@@ -1,10 +1,15 @@
 package com.example.finalEclips.eclips.bizconfig.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.finalEclips.eclips.bizconfig.dto.CreateApprovalDto;
 import com.example.finalEclips.eclips.bizconfig.dto.CreateGpbDto;
 import com.example.finalEclips.eclips.bizconfig.repository.BizImgConfigMapper;
+import com.example.finalEclips.eclips.common.dto.FileDto;
+import com.example.finalEclips.eclips.helper.FileHelper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BizImgConfigServiceImpl implements BizImgConfigService{
 	private final BizImgConfigMapper bizImgConfigMapper;
+	private final FileHelper fileHelper;
 	
 	@Override
 	public int createGpb(CreateGpbDto gpbDto) {
@@ -22,6 +28,14 @@ public class BizImgConfigServiceImpl implements BizImgConfigService{
 	@Override
 	public void createApproval(CreateApprovalDto createApprovalDto) {
 		bizImgConfigMapper.saveApproval(createApprovalDto);
+	}
+
+	@Override
+	public void createApprovalAttachment(int stortId, List<MultipartFile> files) {
+		List<FileDto> fileDtos = fileHelper.uploadFiles(files);
+		bizImgConfigMapper.saveBizImgAttachments(stortId, fileDtos);
+        
+		
 	}
 	
 
